e55cdec4d3e03e0dd2da2b331900541ad67ad29e
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2023-09-26 10:44:10 +0530

    added ip details

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
-})
+    const clientIP = req.ip; // Get client's IP address
+    const frwdedIP = req.headers["x-forwarded-for"];
+    console.log("🚀 ~ file: server.js:7 ~ app.get ~ frwdedIP:", frwdedIP);
+    const connIP = req.connection.remoteAddress;
+    console.log("🚀 ~ file: server.js:9 ~ app.get ~ connIP:", connIP);
+  
+    console.log("🚀 ~ file: server.js:6 ~ app.get ~ clientIP:", clientIP);
+    res.send(`Client IP : ${clientIP} | fwdedIP : ${frwdedIP} | connIP: ${connIP}`);})
 app.listen(process.env.PORT || 3000)
